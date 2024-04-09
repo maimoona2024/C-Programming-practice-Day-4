560d7214da4f1148b117781f51d696d7fc6686a1
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-09 18:26:58 +0500

    Add files via upload

---
+++ Day4.cpp
@@ -0,0 +1,23 @@
+//Program # 1 of Day 4: Area of a Circle.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     float r = 3.2;
+//     float pi = 3.142;
+//     float a = pi*r*r; //formula to calculate area of circle.
+//     cout<<"The Area of a circle having radius = "<< r <<" is "<< a<< ".";
+// }
+
+//Program # 2 of Day 4: Calculator to find Area of a Circle by taking radius of circle from user.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     float r;
+//     float pi = 3.142;
+//     cout<<"Please Enter the value of radius of a Circle: "<<endl;
+//     cin>>r;
+//     float a = pi*r*r;
+//     cout<<"The Area of a circle having radius = "<< r <<" is "<< a<< ".";
+// }
